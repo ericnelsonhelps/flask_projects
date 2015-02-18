c924e6392f61cc96b7d7642624418b272a39ffdf
Eric Nelson <ednelson23@gmail.com>
2015-02-18 03:36:12 +0000

    added additional views

--- flask_hello_world/app.py
+++ flask_hello_world/app.py
@@ -9,10 +9,39 @@ app = Flask(__name__)
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
-
-# define the view using a function, which returns a string
 def hello_world():
 	return "Hello, World!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+# integer route
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
+
+# float route
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+
+# view to test HTTP response codes
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "eric":
+		return "Hello, {}".format(name.capitalize()), 200
+	else:
+		return "Not Found", 404
 
 # starts the development server using the run() method
 if __name__ == "__main__":
